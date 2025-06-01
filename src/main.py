e2f0f6554715fffba277df373721b8dbb6d81324
Guillermo Diván <guillermodivan@hotmail.com>
2025-06-01 19:26:48 -0300

    Started mock menu, connected mysql, user_repo added, user_serv modified

--- src/main.py
+++ src/main.py
@@ -1,17 +1,19 @@
 from services.User_service import UserService
+from repositories.User_repository import UserRepository
 from models.User_role import RoleEnum
 
 def main():
-    us = UserService()
-    new_user = us.register("Pepe", "Argento", "pepe@pepe.com", "pepe1234", RoleEnum.TENANT)
-    print(new_user.user_id)
-    find_user_by_id = us.get_user_by_id(new_user.user_id)
-    print(find_user_by_id.user_id)
-    find_users_by_role = us.get_all_users_by_role(RoleEnum.TENANT)
-    print(len(find_users_by_role))
-    change_user_role = us.change_user_role(new_user.user_id, RoleEnum.ADMIN)
-    print(new_user.role)
-    print(change_user_role)
+    us = UserService(UserRepository())
+    #find_user_by_id = us.get_user_by_id('6e2f98ea-17c1-4e41-86a0-6f819d6203ac')
+    #print(find_user_by_id)
+    #find_users_by_role = us.get_all_users_by_role(RoleEnum.TENANT)
+    #print(len(find_users_by_role))
+    #find_user_by_email = us.get_user_by_email('lilili@lilili.com')
+    #print(find_user_by_email)
+    #new_user = us.register("Roberto", "Garcia", "robertogarcia2@gmail.com", "pepe1234", RoleEnum.TENANT)
+    #print(new_user)
+    #changed_role = us.change_user_role('6e2f98ea-17c1-4e41-86a0-6f819d6203ac', RoleEnum.LANDLORD)
+    #print(changed_role)
 
 
 if __name__ == "__main__":
